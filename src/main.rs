912212306815dfc0d8f5fd92dbcf062365658b50
Bart Slinger <bartslinger@gmail.com>
2024-11-06 17:38:33 +0800

    working on state derivative calculation

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod rigid_body;
 
 fn main() {
     let gravity_model = gravity_model::GravityModel {};
-    let mut body = RigidBody::new(1.0, gravity_model);
+    let mass = 1.0;
+    let inertia = nalgebra::Matrix3::new(0.0135, 0.0, 0.0, 0.0, 0.0010, 0.0, 0.0, 0.0, 0.0142);
+    let mut body = RigidBody::new(gravity_model, mass, inertia);
     let next_state = body.step(&(), 0.01);
     println!("{:?}", next_state);
 }
--- src/rigid_body.rs
+++ src/rigid_body.rs
@@ -29,20 +29,24 @@ pub trait DynamicsModel {
 
 pub struct RigidBody<M: DynamicsModel> {
     mass: f64,
-    inertia: f64,
+    inertia: nalgebra::Matrix3<f64>,
+    inertia_inverse: nalgebra::Matrix3<f64>,
     dynamics_model: M,
     state: State,
 }
 
 impl<M: DynamicsModel> RigidBody<M> {
-    pub fn new(mass: f64, dynamics_model: M) -> Self {
+    pub fn new(dynamics_model: M, mass: f64, inertia: nalgebra::Matrix3<f64>) -> Self {
         let state = nalgebra::SVector::<f64, 13>::from([
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         ]);
 
+        let inertia_inverse = inertia.try_inverse().unwrap();
+
         Self {
             mass,
-            inertia: 10.0,
+            inertia,
+            inertia_inverse,
             dynamics_model,
             state,
         }
@@ -53,14 +57,34 @@ impl<M: DynamicsModel> RigidBody<M> {
         state: &State,
         control_input: &M::ControlInput,
     ) -> StateDerivative {
-        let (Fx, Fy, Fz, Mx, My, Mz) = self
+        let (p, q, r) = (self.state[0], self.state[1], self.state[2]);
+        let (u, v, w) = (self.state[3], self.state[4], self.state[5]);
+        let (q0, q1, q2, q3) = (self.state[6], self.state[7], self.state[8], self.state[9]);
+        let (pn, pe, pd) = (self.state[10], self.state[11], self.state[12]);
+
+        let (force_x, force_y, force_z, moment_x, moment_y, moment_z) = self
             .dynamics_model
             .compute_forces_and_moments(self.state, control_input);
+        let moments = nalgebra::Vector3::new(moment_x, moment_y, moment_z);
 
-        let w_dot = Fz / self.mass;
+        let omega = nalgebra::Vector3::new(p, q, r);
+        let part1 = self.inertia * omega;
+        let coriolis = omega.cross(&part1);
+        let omega_dot = self.inertia_inverse * (moments - coriolis);
+
+        let u_dot = force_x / self.mass - w * q + v * r;
+        let v_dot = force_y / self.mass - u * r + w * p;
+        let w_dot = force_z / self.mass - v * p + u * q;
+        let p_dot = omega_dot[0];
+        let q_dot = omega_dot[1];
+        let r_dot = omega_dot[2];
+        let q0_dot = -0.5 * (q1 * p + q2 * q + q3 * r);
+        let q1_dot = 0.5 * (q0 * p + q2 * r - q3 * q);
+        let q2_dot = 0.5 * (q0 * q + q3 * p - q1 * r);
+        let q3_dot = 0.5 * (q0 * r + q1 * q - q2 * p);
 
         let state_derivative: StateDerivative = nalgebra::SVector::<f64, 13>::from([
-            0.0, 0.0, w_dot, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            u_dot, v_dot, w_dot, p_dot, q_dot, r_dot, q0_dot, q1_dot, q2_dot, q3_dot, 0.0, 0.0, 0.0,
         ]);
         state_derivative
     }
